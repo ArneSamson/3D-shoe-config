79bf2abab9a0bd69c13dcf81f5d51b0c4e1b00c6
ArneSamson <arnesamson007@gmail.com>
2023-11-13 20:10:26 +0100

    feat: add Menu component to app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import Header from './components/Header.vue';
 import Config from './components/Config.vue';
+import Menu from './components/SelectionMenu.vue';
 </script>
 
 <template>
   <Header />
 
   <Config />
+
+  <Menu />
 </template>
 
 <style scoped>
