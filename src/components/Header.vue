619ed5091745d5cce8a70dafceb3caa749025d4f
ArneSamson <arnesamson007@gmail.com>
2023-11-13 19:55:45 +0100

    feat: construct header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <header>
+
+        <div class="shoe-desc">
+            <H1>AIR REV. NITRO S</H1>
+            <p>€ 200</p>
+        </div>
+        
+        <button>
+            <router-link to="/">Done</router-link>
+        </button>
+
+
+    </header>
+
+  </template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
