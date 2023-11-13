d6527e2132c885ee5b433561c62ca6ba0aa4b881
ArneSamson <arnesamson007@gmail.com>
2023-11-13 19:59:04 +0100

    feat: add styling to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,3 +21,61 @@ export default {
   name: 'Header',
 };
 </script>
+
+<style scoped>
+header {
+    background-color: #fff;
+    color: #000;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100vw;
+    margin: 0;
+    border-bottom: #000;
+    border-width: 0 0 2px 0;
+    border-style: solid;
+  }
+
+  .shoe-desc{
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    padding: 43px;
+    background-color: #D6FF38;
+  }
+  
+
+  h1{
+    color: #000;
+    font-size: 1.5rem;
+    margin: 0;
+    font-family: 'cooper-black-std', serif;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: 0.6px;
+
+  }
+  p{
+    font-family: 'inter', sans-serif;
+    font-size: 1§px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+  }
+
+  button{
+    color: #D6FF38;
+    background-color: #000;
+    margin: 43px;
+    width: 300px;
+    height: 68px;
+    font-family: 'inter', sans-serif;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+
+  }
+</style>
