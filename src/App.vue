9d69e4f355b62e7d942738af68cdd58332ba86e8
ArneSamson <arnesamson007@gmail.com>
2023-11-13 20:02:15 +0100

    feat: add config component to app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from './components/Header.vue';
+import Config from './components/Config.vue';
 </script>
 
 <template>
   <Header />
 
+  <Config />
 </template>
 
 <style scoped>
