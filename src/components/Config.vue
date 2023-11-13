92fdd64884b18170fca4df6900ed618e23dfcddf
ArneSamson <arnesamson007@gmail.com>
2023-11-13 20:01:51 +0100

    feat: construct Config component with ThreeJS

---
+++ src/components/Config.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="canvas-container" ref="canvasContainer"></div>
+</template>
+
+<script>
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { TextureLoader } from 'three';
+import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+
+</script>
