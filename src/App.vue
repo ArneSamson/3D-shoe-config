65f21a587ed26001050d897545bb423ae002e4fd
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-15 15:17:22 +0100

    feat: remove unused component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
-import Header from './components/Header.vue';
-import Config from './components/Config.vue';
-import Menu from './components/SelectionMenu.vue';
+import Header from "./components/Header.vue";
+import Config from "./components/Config.vue";
 </script>
 
 <template>
   <Header />
-
   <Config />
-
-  <Menu />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
