b3e08a1a1c6cacf5eb41ad3fcfc97da8eca25f2a
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-17 14:05:23 +0100

    fix: change path to model for vercel

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -346,7 +346,7 @@ export default {
 
     let shoe;
 
-    gltfLoader.load("src/assets/models/new-shoe.glb", (gltf) => {
+    gltfLoader.load("/models/new-shoe.glb", (gltf) => {
       shoe = gltf.scene;
       shoe.scale.set(2.5, 2.5, 2.5);
 
