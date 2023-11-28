610f7120cb022443d6ff2918a65bb56944280c49
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-28 20:14:43 +0100

    style: remove styling and code that is not used

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,12 @@
 <template>
   <header>
     <div class="shoe-desc">
-      <img src="/media/logo.webp" alt="" />
+      <img src="/media/logo.webp" alt="SWEAR-LOGO" />
     </div>
   </header>
 </template>
 
-<script>
-export default {
-  name: "Header",
-  methods: {
-    //when done is clicked, console log the final options from selection menu component
-    logfunction() {
-      console.log(this.$root.finalOptions);
-    },
-  },
-};
-</script>
+<script></script>
 
 <style scoped>
 header {
@@ -38,38 +28,6 @@ header {
   align-items: start;
   padding: 43px;
   background-color: #d6ff38;
-}
-
-h1 {
-  color: #000;
-  font-size: 1.5rem;
-  margin: 0;
-  font-family: "cooper-black-std", serif;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  letter-spacing: 0.6px;
-}
-p {
-  font-family: "inter", sans-serif;
-  font-size: 1§px;
-  font-style: normal;
-  font-weight: 300;
-  line-height: normal;
-}
-
-button {
-  color: #d6ff38;
-  background-color: #000;
-  margin: 43px;
-  width: 300px;
-  height: 68px;
-  font-family: "inter", sans-serif;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 300;
-  line-height: normal;
 }
 
 img {
