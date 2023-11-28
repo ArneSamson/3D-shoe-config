b396aa9c5e45deeab9fd49dd17e852ad6799958f
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-28 18:12:10 +0100

    feat: update app to import router-view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Header from "./components/Header.vue";
 import Config from "./components/Config.vue";
+import Config2 from "./components/Config2.vue";
 </script>
 
 <template>
   <Header />
-  <Config />
+  <router-view />
 </template>
 
 <style scoped></style>
