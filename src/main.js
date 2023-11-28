91944af9ebe472de1de77e0672daf4e534c65f2c
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-28 18:13:01 +0100

    feat: add router to mainjs to use in project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router.js";
 
 const app = createApp(App);
-
-app.mount('#app');
+app.use(router);
+app.mount("#app");
