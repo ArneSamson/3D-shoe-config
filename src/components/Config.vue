e7a355b51279aa0d5cf0d0c4b713f375fa26122c
Evi Vermeêren <evivermeeren@hotmail.com>
2023-12-06 12:07:15 +0100

    feat: finished button appears when configuration is complete

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -127,7 +127,7 @@
       </a>
     </div>
 
-    <button class="configurator__button" @click="goToInfo">
+    <button v-if="progressState" class="configurator__button" @click="goToInfo">
       I'm finished!
     </button>
 
