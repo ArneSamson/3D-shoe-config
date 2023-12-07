b5b53b66a8c7514b19c7463de8895d144c1daf26
Evi Vermeêren <evivermeeren@hotmail.com>
2023-12-07 21:25:45 +0100

    feat: add button to trigger sound

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,10 +3,45 @@
     <div class="shoe-desc">
       <img src="/media/logo.webp" alt="SWEAR-LOGO" />
     </div>
+    <button @click="toggleSound">
+      <img class="shoe-desc__img" src="/media/sound.svg" alt="" />
+    </button>
   </header>
 </template>
 
-<script></script>
+<script>
+import { Howl } from "howler";
+
+export default {
+  data() {
+    return {
+      sound: null,
+      isPlaying: false,
+    };
+  },
+  methods: {
+    initSound() {
+      this.sound = new Howl({
+        src: ["/media/sonicbranding.mp3"], // Replace with the path to your sound file
+        loop: true, // Set to true if you want the sound to loop
+      });
+    },
+    toggleSound() {
+      if (!this.sound) {
+        this.initSound();
+      }
+
+      if (this.isPlaying) {
+        this.sound.pause();
+      } else {
+        this.sound.play();
+      }
+
+      this.isPlaying = !this.isPlaying;
+    },
+  },
+};
+</script>
 
 <style scoped>
 header {
@@ -30,5 +65,27 @@ header {
 
 img {
   width: 200px;
+}
+
+.shoe-desc__img {
+  height: 50px;
+  width: 50px;
+  margin-top: 10px;
+}
+
+button {
+  color: var(--color-accent);
+  background-color: transparent;
+  border: none;
+  width: 50px;
+  height: 50px;
+  font-size: 40px;
+  display: block;
+  margin: auto;
+  margin-right: 50px;
+}
+
+button:hover {
+  cursor: pointer;
 }
 </style>
