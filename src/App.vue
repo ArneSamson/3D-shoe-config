816bf460acd7639ebf38e01db910f9c003d1e4dd
ArneSamson <arnesamson007@gmail.com>
2023-12-10 18:30:54 +0100

    feat: use route to display which header needs to be used

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
+import { useRoute } from "vue-router";
 import Header from "./components/Header.vue";
+import HeaderMain from "./components/HeaderMain.vue";
 import Config from "./components/Config.vue";
 import Config2 from "./components/Config2.vue";
 import Menu from "./components/Menu.vue";
+
+const route = useRoute();
 </script>
 
 <template>
-  <Header />
+  <Header v-if="route.path !== '/'"/>
+  <HeaderMain v-else/>
   <router-view />
 </template>
 
