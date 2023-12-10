35b4006cb2ed4bd43f56a72107313d015f6b9e9d
ArneSamson <arnesamson007@gmail.com>
2023-12-10 18:21:05 +0100

    feat: add a different header for the menu

---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,92 @@
+<template>
+    <header class="headerMain">
+      <router-link to="/">
+        <div class="headerMain__logo">
+          <img
+            class="headerMain__img"
+            src="../assets/SWEAR_Logo.webp"
+            alt="SWEAR-LOGO"
+          />
+        </div>
+      </router-link>
+      <router-link to="/login">
+        <button class="headerMain__admin-button" href="">Admin</button>
+      </router-link>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: "Header",
+  };
+  </script>
+  
+  <style scoped>
+  .headerMain {
+    background-color: #242424;
+    color: #000;
+    display: flex;
+    justify-content: space-between;
+    width: 100vw;
+    margin: 0;
+    border-bottom: #d6ff38;
+    border-width: 0 0 2px 0;
+    border-style: solid;
+    height: 122px;
+  }
+  
+  .headerMain__logo {
+    flex-direction: column;
+    align-items: start;
+    padding: 43px;
+    background-color: #d6ff38;
+  }
+  
+  a {
+    text-decoration: none;
+    color: white;
+  }
+  
+  .headerMain__admin-button {
+    color: #d6ff38;
+    background-color: #000;
+    width: 80%;
+    max-width: 300px;
+    height: 68px;
+    font-family: "cooper-black-std", serif;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+    display: block;
+    margin-right: 100px;
+    margin-top: 30px;
+  }
+  
+  @media (max-width: 600px) {
+    .headerMain {
+      height: 63px;
+    }
+    .headerMain__logo {
+      flex-direction: column;
+      align-items: start;
+      padding: 20px;
+      background-color: #d6ff38;
+    }
+  
+    .headerMain__admin-button {
+      width: 60%;
+      max-width: 200px;
+      height: 30px;
+      font-size: 12px;
+      margin-right: 40px;
+      margin-top: 20px;
+      margin-bottom: 0px;
+    }
+  
+    .headerMain__img {
+      width: 100px;
+    }
+  }
+  </style>
+  
