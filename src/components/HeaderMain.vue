c0f14519a7827b2f60336db7c1c89d5cb4644461
ArneSamson <arnesamson007@gmail.com>
2023-12-10 18:32:28 +0100

    chore: remove unnecessary code and change link to logo img

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -4,13 +4,10 @@
         <div class="headerMain__logo">
           <img
             class="headerMain__img"
-            src="../assets/SWEAR_Logo.webp"
+            src="/media/logo.webp"
             alt="SWEAR-LOGO"
           />
         </div>
-      </router-link>
-      <router-link to="/login">
-        <button class="headerMain__admin-button" href="">Admin</button>
       </router-link>
     </header>
   </template>
