c9d442d58e63fdbc082f152a4b922d3a5b406210
Evi Vermeêren <evivermeeren@hotmail.com>
2023-12-13 15:01:03 +0100

    feat: add thankyoupage to router

--- src/router.js
+++ src/router.js
@@ -3,12 +3,16 @@ import { createRouter, createWebHistory } from "vue-router";
 import Config from "./components/Config.vue";
 import Config2 from "./components/Config2.vue";
 import Menu from "./components/Menu.vue";
+import ThankYou from "./components/ThankYouPage.vue";
 
 const routes = [
   { path: "/", component: Menu },
   { path: "/config", component: Config },
   { path: "/config2", component: Config2 },
-
+  {
+    path: "/thankyou",
+    component: ThankYou,
+  },
 ];
 
 const router = createRouter({
