182fe3e2f6ec548409f721c92337c7b10e4e7389
Evi Vermeêren <evivermeeren@hotmail.com>
2023-12-13 11:46:28 +0100

    feat: load 3D models in overview page

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,268 +1,271 @@
 <template>
-    <div class="models-container">
-      <h1 id="ourmodels" class="models-container__title">Our models</h1>
-      <div class="model flex">
-        <div>
-          <div class="canvas-container" ref="canvasContainer1"></div>
-        </div>
-        <div>
-          <h1 class="model__name">AIR REV. NITRO S</h1>
-          <h2 class="model__type">Custom shoes</h2>
-          <p class="model__price">€200,00</p>
-          <div class="model__buttons buttons">
-            <button>
-              <router-link to="/config" class="buttons__link"
-                >CUSTOMIZE</router-link
-              >
-            </button>
-          </div>
-        </div>
+  <div class="models-container">
+    <h1 id="ourmodels" class="models-container__title">Our models</h1>
+    <div class="model flex">
+      <div>
+        <div class="canvas-container" ref="canvasContainer1"></div>
       </div>
-  
-      <div class="model flex">
-        <div>
-          <div class="canvas-container" ref="canvasContainer2"></div>
-        </div>
-        <div>
-          <h1 class="model__name">AIR REV. XTRA</h1>
-          <h2 class="model__type">Custom shoes</h2>
-          <p class="model__price">€180,00</p>
-          <div class="model__buttons buttons">
-            <button>
-              <router-link to="/config2" class="buttons__link"
-                >CUSTOMIZE</router-link
-              >
-            </button>
-          </div>
+      <div>
+        <h1 class="model__name">AIR REV. NITRO S</h1>
+        <h2 class="model__type">Custom shoes</h2>
+        <p class="model__price">€200,00</p>
+        <div class="model__buttons buttons">
+          <button>
+            <router-link to="/config" class="buttons__link"
+              >CUSTOMIZE</router-link
+            >
+          </button>
         </div>
       </div>
     </div>
+
+    <div class="model flex">
+      <div>
+        <div class="canvas-container" ref="canvasContainer2"></div>
+      </div>
+      <div>
+        <h1 class="model__name">AIR REV. XTRA</h1>
+        <h2 class="model__type">Custom shoes</h2>
+        <p class="model__price">€180,00</p>
+        <div class="model__buttons buttons">
+          <button>
+            <router-link to="/config2" class="buttons__link"
+              >CUSTOMIZE</router-link
+            >
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-  
+
 <script>
-  import * as THREE from "three";
-  import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-  import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-  export default {
-      name: "Menu",
-      mounted() {
-      const windowWidth = window.innerWidth;
-      const squareSize = windowWidth * 0.4;
+export default {
+  name: "Menu",
+  mounted() {
+    const windowWidth = window.innerWidth;
+    const squareSize = windowWidth * 0.4;
 
-      const createScene = (container, modelPaths) => {
-          const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(100, 1, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(squareSize, squareSize);
-        renderer.setPixelRatio(window.devicePixelRatio);
-        container.appendChild(renderer.domElement);
-        camera.position.z = 5;
+    const createScene = (container, modelPath) => {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(100, 1, 0.1, 1000);
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(squareSize, squareSize);
+      renderer.setPixelRatio(window.devicePixelRatio);
+      container.appendChild(renderer.domElement);
+      camera.position.z = 5;
 
-        const loadingManager = new THREE.LoadingManager();
+      const loadingManager = new THREE.LoadingManager();
 
-        const gltfLoader = new GLTFLoader(loadingManager);
+      const gltfLoader = new GLTFLoader(loadingManager);
 
-        const controls = new OrbitControls(camera, renderer.domElement);
-        controls.maxPolarAngle = Math.PI / 2;
-        controls.enablePan = false;
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.maxPolarAngle = Math.PI / 2;
+      controls.enablePan = false;
 
-        scene.background = new THREE.Color(0x242424);
+      scene.background = new THREE.Color(0x242424);
 
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1.7);
-        const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1.7);
-        const directionalLight3 = new THREE.DirectionalLight(0xffffff, 1.7);
-        const directionalLight4 = new THREE.DirectionalLight(0xffffff, 1);
-        directionalLight.position.set(0, 0, 1);
-        directionalLight2.position.set(0, 0, -1);
-        directionalLight3.position.set(0, 1, 0);
-        directionalLight4.position.set(-1, 0, 0);
-        scene.add(directionalLight);
-        scene.add(directionalLight2);
-        scene.add(directionalLight3);
-        scene.add(directionalLight4);
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 1.7);
+      const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1.7);
+      const directionalLight3 = new THREE.DirectionalLight(0xffffff, 1.7);
+      const directionalLight4 = new THREE.DirectionalLight(0xffffff, 1);
+      directionalLight.position.set(0, 0, 1);
+      directionalLight2.position.set(0, 0, -1);
+      directionalLight3.position.set(0, 1, 0);
+      directionalLight4.position.set(-1, 0, 0);
+      scene.add(directionalLight);
+      scene.add(directionalLight2);
+      scene.add(directionalLight3);
+      scene.add(directionalLight4);
 
-        let shoe;
+      let shoe;
 
-        gltfLoader.load(modelPath, (gltf) => {
+      gltfLoader.load(modelPath, (gltf) => {
+        shoe = gltf.scene;
+        shoe.rotation.order = "YXZ";
 
-          shoe = gltf.scene;
-          shoe.rotation.order = "YXZ";
+        if (modelPath === "/models/new-shoe.glb") {
+          shoe.scale.set(2, 2, 2);
+          shoe.rotation.x = 0.5;
+          shoe.rotation.y = 1.5;
+          shoe.position.z = 0;
+          shoe.position.y = -0.5;
+          shoe.position.x = -0.5;
+          scene.add(shoe);
+        } else {
+          shoe.scale.set(0.75, 0.75, 0.75);
+          shoe.rotation.x = 0;
+          shoe.rotation.y = -0.15;
+          shoe.rotation.z = -0.4;
+          shoe.position.z = 0;
+          shoe.position.y = -0.5;
+          shoe.position.x = -0.5;
+          scene.add(shoe);
+        }
+      });
 
-          if(modelPath === "/models/new-shoe.glb") {
-            shoe.scale.set(2, 2, 2);
-            shoe.rotation.x = 0.5;
-            shoe.rotation.y = 1.5;
-            shoe.position.z = 0;
-            shoe.position.y = -0.5;
-            shoe.position.x = -0.5;
-            scene.add(shoe);
-          } else {
-            shoe.scale.set(0.75, 0.75, 0.75);
-            shoe.rotation.x = 0;
-            shoe.rotation.y = -0.15;
-            shoe.rotation.z = -0.4;
-            shoe.position.z = 0;
-            shoe.position.y = -0.5;
-            shoe.position.x = -0.5;
-            scene.add(shoe);
-          }
-        });
+      const animate = () => {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+      };
 
-        const animate = () => {
-          requestAnimationFrame(animate);
-          renderer.render(scene, camera);
-        };
+      animate();
+    };
 
-        animate();
-      }
-      },
-  }
+    // Load the first model in the first container
+    createScene(this.$refs.canvasContainer1, "/models/new-shoe.glb");
 
+    // Load a different model in the second container
+    createScene(this.$refs.canvasContainer2, "/models/vans-shoe.glb");
+  },
+};
 </script>
-  
+
 <style scoped>
-  .models-container {
-    background-color: #242424;
-  }
-  
-  a {
-    text-decoration: none;
-    color: #d6ff38;
-  }
-  
+.models-container {
+  background-color: #242424;
+}
+
+a {
+  text-decoration: none;
+  color: #d6ff38;
+}
+
+.models-container__title {
+  font-family: "cooper-black-std", serif;
+  font-weight: 400;
+  color: white;
+  font-size: 36px;
+  letter-spacing: 1.08px;
+  line-height: normal;
+  margin: 0;
+  padding-top: 50px;
+  margin-left: 50px;
+}
+
+.model {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  width: 100vw;
+}
+
+img {
+  width: 25vw;
+}
+
+.model__name,
+.model__type,
+.model__price {
+  font-family: "cooper-black-std", serif;
+  font-weight: 400;
+  color: white;
+}
+
+.model__name {
+  font-size: 36px;
+  letter-spacing: 1.08px;
+  line-height: normal;
+  margin: 0;
+}
+
+.model__type {
+  font-size: 20px;
+  letter-spacing: 0.72px;
+  line-height: normal;
+  white-space: nowrap;
+  margin: 0;
+}
+
+.model__price,
+p {
+  font-family: "basic-sans", sans-serif;
+  font-weight: 400;
+  color: white;
+  font-size: 16px;
+  letter-spacing: 0;
+  line-height: normal;
+  white-space: nowrap;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+button {
+  width: 299px;
+  height: 36px;
+  top: 0;
+  left: 0;
+  background-color: #000000;
+  font-family: "basic-sans", sans-serif;
+  font-weight: 700;
+  color: #d6ff38;
+  font-size: 14px;
+  letter-spacing: 1.61px;
+  line-height: normal;
+}
+
+.buttons__link {
+  text-decoration: none;
+  color: #d6ff38;
+}
+
+@media (max-width: 800px) {
   .models-container__title {
-    font-family: "cooper-black-std", serif;
-    font-weight: 400;
-    color: white;
-    font-size: 36px;
-    letter-spacing: 1.08px;
-    line-height: normal;
+    font-size: 30px;
     margin: 0;
-    padding-top: 50px;
-    margin-left: 50px;
+    padding-top: 25px;
+    margin-left: 25px;
+    margin-bottom: 50px;
   }
-  
+
   .model {
-    display: flex;
-    align-items: center;
+    flex-wrap: wrap;
+    width: 90vw;
     justify-content: left;
-    width: 100vw;
+    margin-left: 25px;
   }
-  
-  img {
-    width: 25vw;
-  }
-  
-  .model__name,
-  .model__type,
-  .model__price {
-    font-family: "cooper-black-std", serif;
-    font-weight: 400;
-    color: white;
-  }
-  
+
   .model__name {
-    font-size: 36px;
-    letter-spacing: 1.08px;
-    line-height: normal;
-    margin: 0;
+    font-size: 24px;
   }
-  
-  .model__type {
+  button {
+    margin-bottom: 50px;
+  }
+}
+
+@media (max-width: 400px) {
+  .models-container__title {
+    font-size: 24px;
+    padding-top: 20px;
+    margin-left: 20px;
+    margin-bottom: 20px;
+  }
+
+  .model__name {
     font-size: 20px;
-    letter-spacing: 0.72px;
-    line-height: normal;
-    white-space: nowrap;
-    margin: 0;
   }
-  
+
+  .model__type {
+    font-size: 16px;
+  }
+
   .model__price,
   p {
-    font-family: "basic-sans", sans-serif;
-    font-weight: 400;
-    color: white;
     font-size: 16px;
-    letter-spacing: 0;
-    line-height: normal;
-    white-space: nowrap;
   }
-  
-  .buttons {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-  }
-  
+
   button {
-    width: 299px;
-    height: 36px;
-    top: 0;
-    left: 0;
-    background-color: #000000;
-    font-family: "basic-sans", sans-serif;
-    font-weight: 700;
-    color: #d6ff38;
-    font-size: 14px;
-    letter-spacing: 1.61px;
-    line-height: normal;
+    width: 150px;
+    font-size: 12px;
+    margin-bottom: 50px;
   }
-  
-  .buttons__link {
-    text-decoration: none;
-    color: #d6ff38;
-  }
-  
-  @media (max-width: 800px) {
-    .models-container__title {
-      font-size: 30px;
-      margin: 0;
-      padding-top: 25px;
-      margin-left: 25px;
-      margin-bottom: 50px;
-    }
-  
-    .model {
-      flex-wrap: wrap;
-      width: 90vw;
-      justify-content: left;
-      margin-left: 25px;
-    }
-  
-    .model__name {
-      font-size: 24px;
-    }
-    button {
-      margin-bottom: 50px;
-    }
-  }
-  
-  @media (max-width: 400px) {
-    .models-container__title {
-      font-size: 24px;
-      padding-top: 20px;
-      margin-left: 20px;
-      margin-bottom: 20px;
-    }
-  
-    .model__name {
-      font-size: 20px;
-    }
-  
-    .model__type {
-      font-size: 16px;
-    }
-  
-    .model__price,
-    p {
-      font-size: 16px;
-    }
-  
-    button {
-      width: 150px;
-      font-size: 12px;
-      margin-bottom: 50px;
-    }
-  }
+}
 </style>
-  
